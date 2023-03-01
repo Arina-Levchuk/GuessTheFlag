782f4f90d117088b197221953c8aeb59673414d1
Arina Levchuk <aryna.liauchuk@awtg.co.uk>
2023-03-01 18:06:13 +0300

    UI and logic implementation

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Arina Levchuk on 28.02.23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
